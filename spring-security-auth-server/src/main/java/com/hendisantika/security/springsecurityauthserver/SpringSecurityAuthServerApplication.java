a95251df74ce36a4da2f52c0c488f86545ddb12b
Hendi Santika <hendisantika@gmail.com>
2017-10-10 06:30:51 +0700

    #5 - Added Main App

---
+++ spring-security-auth-server/src/main/java/com/hendisantika/security/springsecurityauthserver/SpringSecurityAuthServerApplication.java
@@ -0,0 +1,22 @@
+package com.hendisantika.security.springsecurityauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/10/17
+ * Time: 05.42
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+public class SpringSecurityAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthServerApplication.class, args);
+	}
+}
